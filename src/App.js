893241c79b605df57f107867d9e623dbc68daecd
JaswantsinghShekhawat007 <56820958+JaswantsinghShekhawat007@users.noreply.github.com>
2021-03-13 23:28:21 +0530

    started the project with create-react-app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div> 
+
+      <HomePage /> 
+      
+    </div>
+    
+  );
+}
+
+export default App;

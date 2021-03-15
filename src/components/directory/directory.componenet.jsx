92dd911c9d09076822cc19e0adae2427bbfe8da8
JaswantsinghShekhawat007 <56820958+JaswantsinghShekhawat007@users.noreply.github.com>
2021-03-15 13:41:59 +0530

    updating our app with shop page which has collection-preview componenet which have collection-item component.

--- src/components/directory/directory.componenet.jsx
+++ src/components/directory/directory.componenet.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
+import SECTION_DATA from './sections.data';
 
 import './directory.styles.scss';
 
@@ -11,42 +12,7 @@ class Directory extends React.Component {
 
         this.state = {
 
-            sections : [
-                {
-                  title: 'Hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'hats'
-                },
-                {
-                  title: 'Jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'Sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'Womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'Mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-              ]
-
-
+            sections : SECTION_DATA
         }
     }
 
